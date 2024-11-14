1014a0bfcc18469c73e8af4ed80a5574465c65e5
Keith <135153337+keithcho@users.noreply.github.com>
2024-11-14 11:16:22 +0800

    feat: add logger

--- src/main/java/sg/edu/nus/iss/vttp_day11/VttpDay11Application.java
+++ src/main/java/sg/edu/nus/iss/vttp_day11/VttpDay11Application.java
@@ -2,9 +2,18 @@ package sg.edu.nus.iss.vttp_day11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class VttpDay11Application {
+	@Bean
+	public CommonsRequestLoggingFilter log() {
+		CommonsRequestLoggingFilter logger = new CommonsRequestLoggingFilter();
+		logger.setIncludeClientInfo(true);
+		logger.setIncludeQueryString(true);
+		return logger;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VttpDay11Application.class, args);
